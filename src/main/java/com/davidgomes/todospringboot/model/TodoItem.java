cadf5be3185d689b6436f9e48e9627a3cb6381e3
David Gomes <gomesdpro@gmail.com>
2021-02-15 22:27:16 +0100

    feat(TodoItem): create entity
    Signed-off-by: David Gomes <gomesdpro@gmail.com>

---
+++ src/main/java/com/davidgomes/todospringboot/model/TodoItem.java
@@ -0,0 +1,39 @@
+package com.davidgomes.todospringboot.model;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Data
+public class TodoItem {
+
+    @Setter(AccessLevel.NONE)
+    @GeneratedValue
+    @Id
+    private Integer id;
+    @NotNull
+    @Size(min = 3, max = 255)
+    @Column(nullable = false)
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    @NotNull
+    @Column(nullable = false)
+    private TodoItemStatus status = TodoItemStatus.TODO;
+    @NotNull
+    @ManyToOne(optional = false, cascade = CascadeType.REMOVE)
+    private User user;
+
+    public enum TodoItemStatus {
+        TODO,
+        IN_PROGRESS,
+        DONE
+    }
+
+
+}
